7e828f6a09bc794d571009f6cd8846ad382de9ec
doug schlenker <dougschlenker@gmail.com>
2021-05-10 18:37:56 -0400

    Java sliced_bread

---
+++ java/sliced_bread/src/main/java/com/interview/sonder/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.interview.sonder.sliced_bread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlicedBreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlicedBreadApplication.class, args);
+	}
+
+}
