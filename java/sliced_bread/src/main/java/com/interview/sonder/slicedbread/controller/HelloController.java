54e7869897273533ed4dc136c892bf42a0b05ad8
matthew-walters <matthew.walters@sonder.com>
2021-05-13 09:11:18 -0400

    clean up java project

--- java/sliced_bread/src/main/java/com/interview/sonder/slicedbread/controller/HelloController.java
+++ java/sliced_bread/src/main/java/com/interview/sonder/slicedbread/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.interview.sonder.slicedbread;
+package com.interview.sonder.slicedbread.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
